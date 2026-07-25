5979bc9c7a1d1bed067b99c4150db69211068122
LO <ryan.duke360@gmail.com>
2026-07-25 15:54:42 -0700

    feat: integrate Slice 1 entrance room variants and wiring
    Applies the entrance half of 7e256333f: CaveEntranceRoom, ChasmEntranceRoom,
    CircleBasinEntranceRoom, HallwayEntranceRoom and PillarsEntranceRoom, plus the
    variant-selection plumbing on EntranceRoom.
    
    RegularLevel now builds its entrance through EntranceRoom.createEntrance().
    Without that line the five variants would be unreachable.
    
    Room lists and chances arrays match upstream, with array length equal to the
    room count in every case.

---
+++ core/src/main/java/com/qsr/customspd/levels/rooms/standard/entrance/CircleBasinEntranceRoom.java
@@ -0,0 +1,59 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.qsr.customspd.levels.rooms.standard.entrance;
+
+import com.qsr.customspd.levels.Level;
+import com.qsr.customspd.levels.Terrain;
+import com.qsr.customspd.levels.features.LevelTransition;
+import com.qsr.customspd.levels.painters.Painter;
+import com.qsr.customspd.levels.rooms.Room;
+import com.qsr.customspd.levels.rooms.standard.CircleBasinRoom;
+
+public class CircleBasinEntranceRoom extends CircleBasinRoom {
+
+	@Override
+	public float[] sizeCatProbs() {
+		return new float[]{0, 1, 0};
+	}
+
+	@Override
+	public boolean isEntrance() {
+		return true;
+	}
+
+	@Override
+	public void paint(Level level) {
+		super.paint(level);
+
+		int entrance = level.pointToCell(center());
+		Painter.set( level, entrance, Terrain.ENTRANCE );
+
+		level.transitions.add(new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE, 0));
+	}
+
+	@Override
+	public boolean connect(Room room) {
+		//cannot connect to exit, otherwise works normally
+		if (room.isExit())  return false;
+		else                return super.connect(room);
+	}
+}
---
+++ core/src/main/java/com/qsr/customspd/levels/rooms/standard/entrance/HallwayEntranceRoom.java
@@ -0,0 +1,53 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.qsr.customspd.levels.rooms.standard.entrance;
+
+import com.qsr.customspd.levels.Level;
+import com.qsr.customspd.levels.Terrain;
+import com.qsr.customspd.levels.features.LevelTransition;
+import com.qsr.customspd.levels.painters.Painter;
+import com.qsr.customspd.levels.rooms.standard.HallwayRoom;
+import com.watabou.utils.Point;
+
+public class HallwayEntranceRoom extends HallwayRoom {
+
+	@Override
+	public boolean isEntrance() {
+		return true;
+	}
+
+	@Override
+	public void paint(Level level) {
+		super.paint(level);
+
+		int entrance = -1;
+		for ( Point p : getPoints()){
+			if (level.map[level.pointToCell(p)] == Terrain.STATUE_SP){
+				entrance = level.pointToCell(p);
+				break;
+			}
+		}
+		Painter.set( level, entrance, Terrain.ENTRANCE );
+		level.transitions.add(new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE, 0));
+
+	}
+}
